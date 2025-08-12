f7104c482ad4905416f09e6db6a2e5a22359b1ad
Shruzana <152085938+Shruzana@users.noreply.github.com>
2025-08-13 01:46:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import joblib
 model = joblib.load("best_fit_model.pkl")
 
 # Features for prediction
-features = ['Brand', 'MRP', 'RAM', 'ROM', 'Display_Size', 'Battery', 'Front_Cam(MP)', 'Back_Cam(MP)']
+features = ['Brand', 'RAM', 'ROM', 'Display_Size', 'Battery', 'Front_Cam(MP)', 'Back_Cam(MP)']
 
 # App Title
 st.set_page_config(page_title="E-Commerce Smartphone Price Predictor", layout="wide")
@@ -64,3 +64,4 @@ elif choice == "About":
     - **Dataset:** Scraped from Amazon & Flipkart mobile listings  
     - **Goal:** Help users and sellers estimate the selling price of smartphones based on specifications.  
     """)
+
